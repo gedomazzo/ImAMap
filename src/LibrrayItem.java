3b05233212af77f968de8bc817db35446a0a8163
Vasya <br2876@bs.amalnet.k12.il>
2025-11-24 12:53:13 +0200

    the abstract class in the library

--- src/LibrrayItem.java
+++ src/LibrrayItem.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import java.util.UUID;
 
-public class LibrrayItem {
+public abstract class LibrrayItem {
+    private final UUID itemid;
+    private String title;
+    private String year;
+    private boolean is_borrowed;
+
+    LibrrayItem(String title, String year, boolean is_borrowed) {
+        itemid = UUID.randomUUID();
+        this.title = title;
+        this.year = year;
+        this.is_borrowed = is_borrowed;
+    }
+
+    public abstract double calculateLateFee(int daysPastDue);
+
+    public void borrow(){
+        this.is_borrowed = true;
+    }
+
+    public void return_item(){
+        this.is_borrowed = false;
+    }
+
+
+
+    public UUID getItemid() {
+        return itemid;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getYear() {
+        return year;
+    }
+
+    public boolean isBorrowed() {
+        return is_borrowed;
+    }
+
+
+    @Override
+    public String toString(){
+        return  "id: " + itemid + " \n, " +
+                "title: " + title + ",\n" +
+                " year: " + year;
+    }
+
 }
